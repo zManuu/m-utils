ea4c99c088f8327584973b133e20288b1d6ff9af
zManuu <60520810+zManuu@users.noreply.github.com>
2023-05-22 17:14:49 +0200

    [~] removed loggin in auth

--- src/auth.ts
+++ src/auth.ts
@@ -8,29 +8,21 @@ class Authenticator<T, U = number> {
 
   authenticate(key: T, mapping: U) {
     if (this.isAuthenticated(key))
-      return console.warn(`Tried to authenticate ${key}, but they are authenticated already.`)
+      return
 
     this.sessions.set(key, mapping)
-    console.log(`Successfully authenticated ${key} with mapping ${mapping}.`)
   }
 
   removeAuth(key: T) {
-    if (!this.isAuthenticated(key)) {
-      console.warn(`Tried to remove auth of ${key}, but isn't authenticated.`)
+    if (!this.isAuthenticated(key))
       return
-    }
 
-    if (this.sessions.delete(key))
-      console.log(`Successfully removed auth from ${key}.`)
-    else
-      console.warn(`Couldn't remove auth from ${key}.`)
+    this.sessions.delete(key)
   }
 
   getMapping(key: T) {
-    if (!this.isAuthenticated(key)) {
-      console.warn(`Tried to get the mapping of ${key}, but isn't authenticated.`)
-      return
-    }
+    if (!this.isAuthenticated(key))
+      return undefined
 
     return this.sessions.get(key)
   }
