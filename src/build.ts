5f3e83bb47c956caebfffa02418a32ace2c8c5cd
zManuu <60520810+zManuu@users.noreply.github.com>
2023-05-23 14:39:59 +0200

    [+] build-helper

---
+++ src/build.ts
@@ -0,0 +1,35 @@
+// !
+// ! The build-feature is only available via npx.
+// ! It is used to copy asset files.
+// !
+
+import * as fs from 'fs/promises'
+import { join } from 'path'
+
+const passedArguments = process.argv.slice(2).map((arg) => arg.replace('--', ''))
+const config = {
+	from: passedArguments[0],
+	to: passedArguments[1],
+	regex: new RegExp(passedArguments[2])
+}
+
+handleFolder(config.from)
+
+async function handleFolder(folderUrl: string) {
+	const entries = await fs.readdir(folderUrl)
+	for (const entry of entries) {
+		if (entry.includes('.'))
+			handleFile(`${folderUrl}/${entry}`)
+		else
+			handleFolder(`${folderUrl}/${entry}`)
+	}
+}
+
+async function handleFile(fileUrl: string) {
+	if (!config.regex.test(fileUrl))
+		return
+
+	const newPath = join(config.to, fileUrl.substring(0, fileUrl.length))
+
+	fs.copyFile(fileUrl, newPath)
+}
